305e3d5c922b6be85043b3c4ff95765c749675a9
lucasspor <lucassporto.contato@gmail.com>
2025-10-14 07:55:51 -0300

    feat: create function to add new user

--- api/src/controllers/UsersControllers.js
+++ api/src/controllers/UsersControllers.js
@@ -1,6 +1,24 @@
-class UsersController {
-    async create(){
+const AppError = require("../utils/AppErrors")
+const { hash } = require("bcryptjs")
+const sqliteConnection = require("../database/sqlite")
 
+class UsersController {
+    async create(req, res) {
+        const { name, email, password } = req.body
+
+        const database = await sqliteConnection()
+
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if (checkUserExist) {
+            throw new AppError("This email is already in use.")
+        }
+
+        const hashedPassword = await hash(password, 8)
+
+        await database.run("INSERT IN users(name, email, password, VALUES (?,?,?)", [name, email, hashedPassword])
+
+        return res.status(201).json({ message: "This user will be create" })
     }
 }
 
