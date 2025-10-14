c22d622f218132745feea4cdf2dae72ffdf870ca
lucasspor <lucassporto.contato@gmail.com>
2025-10-14 04:02:23 -0300

    feat: using AppError

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,31 @@
 require("express-async-errors")
+
+const AppError = require("./utils/AppErrors")
+
 const express = require("express");
 
 const app = express();
+app.use(express.json())
+
 const port = 3333;
 
-app.get("/", (req, res) =>{
+app.use((error, req, res, next) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message
+        })
+    }
+
+    console.error(error)
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+})
+
+app.get("/", (req, res) => {
     res.send()
 })
 
