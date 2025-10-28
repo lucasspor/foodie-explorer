255486e0ec2f9abc460c8027c0cfecc53914c0e6
lucasspor <lucassporto.contato@gmail.com>
2025-10-28 04:17:07 -0300

    fix(routes): correct wrong export in users.routes

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express")
 const UsersController = require("../controllers/UsersControllers")
-const usersController = UsersController()
+const usersController = new UsersController()
 
 const routes = Router()
 
 routes.use("/", usersController.create)
 routes.use("/", usersController.update)
+
+module.exports = routes
